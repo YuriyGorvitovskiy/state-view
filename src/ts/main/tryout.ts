1acb8bbb25bf76a42aa729bf7e22c406a5022150
Yuriy Gorvitovskiy <yuriy@gorvitovskiy.com>
2018-08-10 12:34:16 -0700

    Adding Actions with mapping to the tryout.

--- src/ts/main/tryout.ts
+++ src/ts/main/tryout.ts
@@ -16,13 +16,44 @@ export interface RequestArray {
     $id: string[] | Path<string[]>;
 }
 
-export type Function<R, T> = (R) => T;
+export interface Function<P, T> {
+    $function: string,
+    $prop: Single<P>,
+};
+
+export interface Mapping<F, T> {
+    $mapping: string,
+};
+
+export function mapping<T>(p: string): Mapping<any, T> {
+    return {
+        $mapping: p
+    };
+};
+
+
 export type Field<R, T> = T | Path<T> | Function<R, T>;
+export type MappedField<F, R, T> = T | Path<T> | Function<R, T> | Mapping<F, T>;
+
 export type Single<T> = T | T & Request;
 export type Plural<T> = T[] | (T & Request)[] | T & RequestArray;
 
+export interface Widget<T> {
+    $widget: string,
+    $prop: Single<T>,
+};
+
+export interface Action<E, P> {
+    $action: string,
+    $prop: Single<P>,
+}
+
 export interface Value<P> {
     value: Field<Value<P>, P>;
+}
+
+export interface ValueEditable<P> extends Value<P> {
+    edit: Action<Value<P>, any>;
 }
 
 export interface Column {
@@ -44,10 +75,10 @@ export interface Table {
     rows: Plural<Row>;
 }
 
-export interface Widget<T> {
-    $widget: string,
-    $prop: Single<T>,
-};
+export interface ChangeAttribute<F, T> {
+    attr:  MappedField<F, ChangeAttribute<F, T>, string>;
+    value: MappedField<F, ChangeAttribute<F, T>, T>;
+}
 
 export function stringWidget(prop: Value<string>): Widget<Value<string>> {
     return {
@@ -56,16 +87,23 @@ export function stringWidget(prop: Value<string>): Widget<Value<string>> {
     };
 };
 
-export function numberWidget(prop: Value<number>): Widget<Value<number>> {
+export function stringEditWidget(prop: ValueEditable<string>): Widget<ValueEditable<string>> {
     return {
-        $widget: "number-value",
+        $widget: "string-edit-value",
         $prop: prop
     };
 };
 
-export function booleanWidget(prop: Value<boolean>): Widget<Value<boolean>> {
+export function numberEditWidget(prop: ValueEditable<number>): Widget<ValueEditable<number>> {
     return {
-        $widget: "boolean-value",
+        $widget: "number-edit-value",
+        $prop: prop
+    };
+};
+
+export function booleanEditWidget(prop: ValueEditable<boolean>): Widget<ValueEditable<boolean>> {
+    return {
+        $widget: "boolean-edit-value",
         $prop: prop
     };
 };
@@ -73,6 +111,13 @@ export function booleanWidget(prop: Value<boolean>): Widget<Value<boolean>> {
 export function tableWidget(prop: Single<Table>):  Widget<Table> {
     return {
         $widget: "table",
+        $prop: prop
+    };
+}
+
+export function changeAttributeAction<E, T>(prop: Single<ChangeAttribute<E, T>>):  Action<E, ChangeAttribute<E, T>> {
+    return {
+        $action: "change_attribite",
         $prop: prop
     };
 }
@@ -92,13 +137,19 @@ const TABLE: Widget<Table> = tableWidget({
         $id: path("^item:list"),
         items: [{
             column_id: "name",
-            widget: stringWidget({value: path("name")})
+            widget: stringEditWidget({
+                value: path("name"),
+                edit: changeAttributeAction({
+                    attr: "name",
+                    value: mapping("value")
+                })
+            })
         },{
             column_id: "boolean",
-            widget: booleanWidget({value: path("boolean")})
+            widget: booleanEditWidget({value: path("boolean")})
         },{
             column_id: "number",
-            widget: numberWidget({value: path("number")})
+            widget: numberEditWidget({value: path("number")})
         }]
     }
 });
